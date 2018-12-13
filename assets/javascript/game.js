cb2d34a7ad01a015425bcc60d0aab7bf1d18b1c5
Diego <didachos24@gmail.com>
2018-12-13 14:08:01 -0600

    Print spaces for word to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,24 @@ var capitals = ["bogota", "medellin", "cali", "barranquilla", "cartagena"];
 var wins = 0;
 var remain = 0;
 var mycity;
+var cityspaces;
 
 // Create variables to hold references
 // var randomCity = document.getElementById("randomCity");
 
-
-document.onkeyup = function(event) {
-
-    // Pick a city
-    var mycity = capitals[Math.floor(Math.random() * capitals.length)];
-    console.log(mycity);
-    console.log(mycity.length)
+// Pick a city
+var mycity = capitals[Math.floor(Math.random() * capitals.length)];
+console.log(mycity);
+console.log(mycity.length)
 
     // Print spaces for guess
     for (i=0; i < mycity.length; i++) {
-        document.getElementById("randomCity.innerHTML") += "_ ";
+        cityspaces = cityspaces + "_ ";
     }
-}
+    console.log(cityspaces);
+    (document).getElementById("spaces") = cityspaces;
+
+
+// document.onkeyup = function(event) {
+
+// }
