f876fabf8f28f60974cc022c08f23d19fb26c4f9
Diego <didachos24@gmail.com>
2018-12-13 23:42:34 -0600

    Print letters tries

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,36 @@ var mycity;
 var cityspaces;
 
 // Create variables to hold references
+    var spaces = document.getElementById("spaces");
+    var remain = document.getElementById("remaining");
+    var guess, sum = 0, tries = "", cityspaces = "";
+
 // var randomCity = document.getElementById("randomCity");
 
 // Pick a city
 var mycity = capitals[Math.floor(Math.random() * capitals.length)];
-console.log(mycity);
-console.log(mycity.length)
+console.log("City to guess: " + mycity);
+console.log("Number of spaces to guess: " + mycity.length)
 
-    // Print spaces for guess
+    // Number of spaces to guess
     for (i=0; i < mycity.length; i++) {
         cityspaces = cityspaces + "_ ";
     }
     console.log(cityspaces);
-    (document).getElementById("spaces") = cityspaces;
+
+document.onkeyup = function(event) {
+    guess = event.key;
+    sum = sum + 1;
+    tries = tries + guess;
+    if(sum < 5) {
+        document.getElementById("spaces").innerHTML =cityspaces;
+        document.getElementById("tries").innerHTML = tries;
+        
+    }
+
+    document.getElementById("spaces").innerHTML = myCity;
+
+    console.log(guess, sum, tries);
 
 
-// document.onkeyup = function(event) {
-
-// }
+}
