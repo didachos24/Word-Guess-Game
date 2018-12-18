60418219a05189d522ab5cdeab0dd1fd7397996b
Diego <didachos24@gmail.com>
2018-12-18 00:05:12 -0600

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var capitals = ["bogota", "medellin", "cali", "barranquilla", "cartagena"];
 
 // Create variables
-var wins = 0, mycity, cityspaces, guess, fail = 0, wrong = "", cityspaces = "", isInCapital = false;
+var wins = 0, mycity, cityspaces = "", guess, fail = 0, wrong = "", cityspaces = "", isInCapital = false;
 var spaces = document.getElementById("tries");
 
 // Pick a city
@@ -11,11 +11,11 @@ console.log("City to guess: " + mycity);
 console.log("Number of spaces to guess: " + mycity.length)
 
 // Number of spaces to guess
-    for (i=0; i < mycity.length; i++) {
-        cityspaces = cityspaces + "_ ";
+    for (i=0; i < mycity.length; i+=1) {
+        cityspaces = cityspaces + "_";
     }
-    console.log(cityspaces);
-    // document.getElementById("spaces") = cityspaces;
+    console.log(cityspaces, cityspaces.length);
+    // document.getElementById("spaces") = cityspaces; // Doesn't show in index*********
 
 // Start the game, pick a letter
     document.onkeyup = function(event) {
@@ -24,14 +24,20 @@ console.log("Number of spaces to guess: " + mycity.length)
         guess = event.key;
 
     // Compare guess with each letter in capital city
-        for (i=0; i < mycity.length; i++) {
-            let = mycity.charAt(i);
+    for (i=0; i < mycity.length; i++) {
+
+// ************ Here is working************
 
         //Is guess in capital city? Yes, print letter in array
-            if( let == guess) {
-                cityspaces = cityspaces.replace(i, let);
+            if(mycity.charAt(i) == guess) {
+                // cityspaces.charAt(i) = guess; // Here is the problem*************
+            } else {
+                wrong += guess;
+                fail += 1;
+                // document.getElementById("wrong") = wrong;
+                console.log(wrong, fail);
             }
-            console.log(cityspaces);
 
+        
         }
     }
