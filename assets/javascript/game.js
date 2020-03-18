edef83cc7d879860946d359c3bb64b101e132f2a
Diego F Montealegre Silva <didachos24@gmail.com>
2020-03-18 12:44:38 -0500

    Alphabetic selection is running, not working multi-character key

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,8 @@ var capitals = [
     "pasto", 
     "pereira", 
     "tunja", 
-    "bucaramanga"];
+    "bucaramanga"
+];
 
 // Variables
 var wins = 0;
@@ -21,68 +22,88 @@ var cityspaces = [];
 var guesses = 0; 
 var fails = 0;
 var wrong = "";
-logic = false;
+var logic = false;
+var alpha = /^[A-Za-z]/;
 
-// Pick a city
+// Pick a city function. Takes a city from cities array
 mycity = capitals[Math.floor(Math.random() * capitals.length)];
+
+// Amount of letters in city 
 letcounter = mycity.length;
-console.log("City: " + mycity);
-console.log("Number of letters: " + mycity.length)
 
-// Number of spaces to guess
-    for (i=0; i < mycity.length; i+=1) {
-        cityspaces.push("__");
-    }
+// Console log variables
+console.log("City: " + mycity + "\nNumber of letters: " + mycity.length);
 
+// Push spaces in an array, each space as an element to fill that indicates the letters to guess
+for (i=0; i < mycity.length; i+=1) {
+    cityspaces.push(" _ ");
+}
 
-// Start the game, pick a letter
-    document.onkeyup = function(event) {
+// Start the game, press a letter
+    document.onkeyup = function(guess) {
 
-    // Change spaces to fill with guessed letters and advice how many letters are
-    document.getElementById("spaces").innerHTML = cityspaces + " Letters to guess " + letcounter; 
+    // Push the array with spaces to guess in HTML 
+    document.getElementById("spaces").innerHTML = cityspaces;
+
+    // Tell how many letters there are to guess
+    document.getElementById("letters").innerHTML = "\n Letters to guess " + letcounter 
 
     // Guess letter as variable
         guess = event.key;
-        guess1 = guess.toLowerCase();
 
-    // Compare guess with each letter in capital city
-    for ( var i = 0 ; i < mycity.length; i++) {
+        // Conditional to check for a letter pressed
+        if(guess.match(alpha) //&& guess.getModifierState()) {
+            ){
 
-        let = mycity.charAt(i);
+            guess = guess.toLowerCase();
 
-        // If guess is in mycity, print 
-
-            if( let == guess) {
+            // Compare guess with each letter in capital city
+            for (var i = 0; i < mycity.length; i++) {
+        
+                let = mycity.charAt(i);
+        
+                // If guess is in mycity, print 
+        
+                    if( let == guess) {
+                        
+                        let = let.toUpperCase();
+                        guesses +=1 ;
+                        logic = true;
+                        cityspaces[i] = let;
+                        letcounter -= 1;
+                        document.getElementById("spaces").innerHTML = cityspaces + " Letters to guess " + letcounter;
+                        if(guesses >= mycity.length) {
+                            document.getElementById("image-result").style.height="400px";
+                            document.getElementById("image-result").style.weight="600px";
+                            document.getElementById("image-result").src = "assets/images/" + mycity + ".jpg";
+                            alert("Congratulations, You Win!!!");
+        
+                            document.getElementById("image-result")
+                        }
                 
-                let = let.toUpperCase();
-                guesses +=1 ;
-                logic = true;
-                cityspaces[i] = let;
-                letcounter -= 1;
-                document.getElementById("spaces").innerHTML = cityspaces + " Letters to guess " + letcounter;
-                if(guesses >= mycity.length) {
-                    document.getElementById("image-result").style.height="400px";
-                    document.getElementById("image-result").style.weight="600px";
-                    document.getElementById("image-result").src = "assets/images/" + mycity + ".jpg";
-                    alert("Congratulations, You Win!!!");
+                    } 
+                } 
+        
+            if ( logic == false ) {  
+                
+                guess = guess.toUpperCase();
+                wrong += guess + " ";
+                fails += 1;
+                document.getElementById("bad-try").innerHTML = "Wrongs: " + wrong;
+                document.getElementById("fails").innerHTML = "Fails: " + fails;
+                
+                if ( fails > 4) {
+                    document.getElementById("image-result").src = "assets/images/hanged.jpg";
+                    alert("Game over!");
                 }
         
-            } 
-        } 
-    if ( logic == false ) {  
+            }
         
-        guess = guess.toUpperCase();
-        wrong += guess + " ";
-        fails += 1;
-        document.getElementById("bad-try").innerHTML = "Wrongs: " + wrong;
-        document.getElementById("fails").innerHTML = "Fails: " + fails;
+                logic = false;
         
-        if ( fails > 4) {
-            document.getElementById("image-result").src = "assets/images/hanged.jpg";
-            alert("Game over!");
-        }
+            
+        } else {
+            alert("Please select a letter")
+        };
 
-    }
-
-        logic = false;
 }
