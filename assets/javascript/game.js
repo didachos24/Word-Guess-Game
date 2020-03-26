880be67250774a3af32e7f5d3312ee36fd5bbafb
Diego F Montealegre Silva <didachos24@gmail.com>
2020-03-26 00:21:46 -0500

    Game.js updated, created conditional to check for used keys

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,6 +24,7 @@ var fails = 0;
 var wrong = "";
 var logic = false;
 var alpha = /^[A-Za-z]/;
+var pressedKeys = [];
 
 // Pick a city function. Takes a city from cities array
 mycity = capitals[Math.floor(Math.random() * capitals.length)];
@@ -32,7 +33,7 @@ mycity = capitals[Math.floor(Math.random() * capitals.length)];
 letcounter = mycity.length;
 
 // Console log variables
-console.log("City: " + mycity + "\nNumber of letters: " + mycity.length);
+// console.log("City: " + mycity + "\nNumber of letters: " + mycity.length);
 
 // Push spaces in an array, each space as an element to fill that indicates the letters to guess
 for (i=0; i < mycity.length; i+=1) {
@@ -40,70 +41,95 @@ for (i=0; i < mycity.length; i+=1) {
 }
 
 // Start the game, press a letter
-    document.onkeyup = function(guess) {
+document.onkeyup = function(guess) {
 
-    // Push the array with spaces to guess in HTML 
+    // Push the array with spaces to guess in index.html 
     document.getElementById("spaces").innerHTML = cityspaces;
 
     // Tell how many letters there are to guess
-    document.getElementById("letters").innerHTML = "\n Letters to guess " + letcounter 
+    document.getElementById("letters").innerHTML = "Letters to guess " + letcounter 
 
     // Guess letter as variable
-        guess = event.key;
+    guess = event.key;
 
-        // Conditional to check for a letter pressed
-        if(guess.match(alpha) //&& guess.getModifierState()) {
-            ){
+    // Conditional to check for a letter pressed, only alphabetic option
+    if(guess.match(alpha) && guess.length == 1) {
 
+        // Check if the key was pressed
+        if(!pressedKeys.includes(guess)) {
+
+            // To lower case to match with city from array
             guess = guess.toLowerCase();
 
             // Compare guess with each letter in capital city
             for (var i = 0; i < mycity.length; i++) {
         
-                let = mycity.charAt(i);
+                let letter = mycity.charAt(i);
         
                 // If guess is in mycity, print 
-        
-                    if( let == guess) {
-                        
-                        let = let.toUpperCase();
-                        guesses +=1 ;
-                        logic = true;
-                        cityspaces[i] = let;
-                        letcounter -= 1;
-                        document.getElementById("spaces").innerHTML = cityspaces + " Letters to guess " + letcounter;
-                        if(guesses >= mycity.length) {
-                            document.getElementById("image-result").style.height="400px";
-                            document.getElementById("image-result").style.weight="600px";
-                            document.getElementById("image-result").src = "assets/images/" + mycity + ".jpg";
-                            alert("Congratulations, You Win!!!");
-        
-                            document.getElementById("image-result")
-                        }
+                if( letter == guess) {
+                    
+                    // letter to upper case to print in index.html
+                    letter = letter.toUpperCase();
+                    // Add 1 to guesses. This variable is used to check final of the game
+                    guesses +=1 ;
+                    logic = true;
+                    // Insert guess in spaces array
+                    cityspaces[i] = letter;
+                    // Insert guess in array to not repeat keys
+                    pressedKeys.push(guess);
+                    // console.log(pressedKeys);
+                    // One less letter to guess
+                    letcounter -= 1;
+                    // Print guess and modify spaces in index.html
+                    document.getElementById("spaces").innerHTML = cityspaces;
+                    // Print amount of letters remaining to guess
+                    document.getElementById("letters").innerHTML = " Letters to guess " + letcounter;
+                    // Conditional that check if user finish game checking if amount of letters in guesses is bigger than micity
+                    // If conditional, user win and print the city
+                    if(guesses >= mycity.length) {
+                        document.getElementById("image-result").style.height="400px";
+                        document.getElementById("image-result").style.weight="600px";
+                        document.getElementById("image-result").src = "assets/images/" + mycity + ".jpg";
+                        alert("Congratulations, You Win!!!");
+                    }
                 
-                    } 
                 } 
-        
+            } 
+
+            // If letter is not in mycity
             if ( logic == false ) {  
-                
+
+                // Insert guess in array to not repeat keys
+                pressedKeys.push(guess);
+                // Guess to upper case 
                 guess = guess.toUpperCase();
+                // Add guess in wrong array to print index.html
                 wrong += guess + " ";
+                // Add one in fails counter to finish game 
                 fails += 1;
+                // Print wrongs in index.html
                 document.getElementById("bad-try").innerHTML = "Wrongs: " + wrong;
-                document.getElementById("fails").innerHTML = "Fails: " + fails;
-                
+
+                // Conditional to check if user losts 
                 if ( fails > 4) {
+                    // If user losts print hanged man and finish game
                     document.getElementById("image-result").src = "assets/images/hanged.jpg";
                     alert("Game over!");
                 }
         
             }
-        
-                logic = false;
-        
-            
         } else {
-            alert("Please select a letter")
-        };
+            // If key was pressed, alert user
+            alert("Key used previously.")
+        }
 
-}
+        // Restart press key event
+        logic = false;
+        
+    } else {
+        // Alert the user to select an alphabetic key
+        alert("Please select a letter.");
+    };
+
+};
